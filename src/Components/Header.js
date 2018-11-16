b1b4bb553f194c077e6c572ab48339a6523ce8d4
Yi <mavisluan83@gmail.com>
2018-11-15 17:30:33 -0800

    add Header

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => (
+    <div className='header'>
+        <img src={require('../icons/logo.png')} width='50px' height='50px' />
+        <div>Book unique homes and expriences.</div>
+    </div>
+)
+
+export default Header
---
+++ src/static-data.js
@@ -0,0 +1,55 @@
+export const data = {
+    explore: [
+        {
+            id: 0,
+            name: 'homes',
+            src: require('./icons/explore-homes.jpg')
+        },
+        {
+            id: 1,
+            name: 'experiences',
+            src: require('./icons/explore-experiences.jpg')
+        },
+        {
+            id: 2,
+            name: 'restaurants',
+            src: require('./icons/explore-restaurants.jpeg')
+        },
+    ],
+    plus: {
+        name: 'plus',
+        src: require('./icons/plus.jpg')
+    },
+    recommended: [
+        { 
+            id: 0, 
+            name: 'Paris',
+            src: require('./icons/paris.jpg'),
+            price: 103
+        },
+        { 
+            id: 1, 
+            name: 'London',
+            src: require('./icons/paris.jpg'),
+            price: 117
+        },
+        { 
+            id: 2, 
+            name: 'San Francisco',
+            src: require('./icons/sanfrancisco.jpg'),
+            price: 156
+        },
+        { 
+            id: 3, 
+            name: 'Beijing',
+            src: require('./icons/beijing.jpg'),
+            price: 90
+        },
+        { 
+            id: 4, 
+            name: 'Barcelona',
+            src: require('./icons/barcelona.jpg'),
+            price: 73
+        },
+    ]
+}
